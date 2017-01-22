e1a1c4fda0c095d817abc68749ba16588e6f1d8f
Alexander Shoup <shoup_alex@yahoo.com>
2017-01-21 16:33:11 -0800

    don't load mtgjson on heroku

--- Gemfile
+++ Gemfile
@@ -7,26 +7,26 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
+gem 'rails'#, '~> 5.0.1'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 # Use Puma as the app server
-gem 'puma', '~> 3.0'
+gem 'puma'#, '~> 3.0'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+gem 'sass-rails'#, '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier'#, '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
+gem 'coffee-rails'#, '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
+gem 'turbolinks'#, '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
-gem 'jbuilder', '~> 2.5'
+gem 'jbuilder'#, '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
@@ -44,10 +44,10 @@ end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
+  gem 'web-console'#, '>= 3.3.0'
   #gem 'sqlite3', '1.3.12'
-  gem 'json'
-  gem 'rubyzip', '>= 1.0.0'
+  #gem 'json'
+  gem 'rubyzip'#, '>= 1.0.0'
 end
 
 #group :production do
--- config/application.rb
+++ config/application.rb
@@ -12,27 +12,29 @@ module MtgCardRepo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
 
-    # Ensure that mtgjson is downloaded - if not, download it
-    mtgjson_file = Rails.root.join('app', 'json', 'mtg.json')
-    mtgjson_zip = Rails.root.join('app', 'json', 'mtg.json.zip')
-    unless File.exist?(mtgjson_file)
-      puts "No MTG JSON file found."
-      unless File.exist?(mtgjson_zip)
-        puts "Downloading from mtgjson.com..."
-        IO.copy_stream(open('http://mtgjson.com/json/AllSets.json.zip'), mtgjson_zip.to_s)
-        until File.exist?(mtgjson_zip) do
-          sleep 2
+    def load_json
+      # Ensure that mtgjson is downloaded - if not, download it
+      mtgjson_file = Rails.root.join('app', 'json', 'mtg.json')
+      mtgjson_zip = Rails.root.join('app', 'json', 'mtg.json.zip')
+      unless File.exist?(mtgjson_file)
+        puts "No MTG JSON file found."
+        unless File.exist?(mtgjson_zip)
+          puts "Downloading from mtgjson.com..."
+          IO.copy_stream(open('http://mtgjson.com/json/AllSets.json.zip'), mtgjson_zip.to_s)
+          until File.exist?(mtgjson_zip) do
+            sleep 2
+          end
+        end
+        puts "Unzipping MTG JSON file..."
+        Zip::File.open(mtgjson_zip) do |zipfile|
+          zipfile.each do |f|
+            zipfile.extract(f, mtgjson_file)
+          end
         end
       end
-      puts "Unzipping MTG JSON file..."
-      Zip::File.open(mtgjson_zip) do |zipfile|
-        zipfile.each do |f|
-          zipfile.extract(f, mtgjson_file)
-        end
-      end
+      puts "Loaded MTG JSON successfully."
     end
-    puts "Loaded MTG JSON successfully."
   end
 end
