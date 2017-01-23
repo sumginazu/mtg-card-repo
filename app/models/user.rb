d288ed37059db5cd38ec342d5221ffd022baac94
Alexander Shoup <shoup_alex@yahoo.com>
2017-01-22 20:55:32 -0800

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, length: { maximum: 12 }, presence: true
+  validates :name,     length: { maximum: 100 }, presence: true
+  validates :email,    presence: true
 end
