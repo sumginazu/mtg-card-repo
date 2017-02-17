f437d466bbb900164208c5c75a960d4270861c52
Alexander Shoup <shoup_alex@yahoo.com>
2017-02-16 22:28:43 -0800

    update page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  #page title
+  def full_title(page_name)
+    base_title = "MTG Card Repo"
+    page_name.empty? ? base_title : "#{page_name} | #{base_title}"
+  end
 end
