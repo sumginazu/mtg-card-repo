ceda129b6a87b16a448f3a7f7309c10eb60b22a0
Alexander Shoup <shoup_alex@yahoo.com>
2017-02-26 23:36:35 -0800

    added (secure?) passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   validates :username, length: { maximum: 20 },  presence: true, uniqueness: { case_sensitive: true }
   validates :name,     length: { maximum: 100 }, presence: true
   validates :email,    length: { maximum: 255 }, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6, maximum: 63 }, presence: true
+
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,58 +2,82 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(username: 'test_user', name: 'Test User', email: 'test@example.com')
+    @user = User.new(username: 'test_user', name: 'Test User', email: 'test@example.com',
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
-    @user = User.new(username: 'test_user', name: 'Test User', email: 'test@example.com')
-    assert @user.valid?
+    a = @user.dup
+    assert a.valid?
   end
 
   test 'should be nonempty' do
-    a = User.new(username: '         ', name: 'Test User', email: 'test@example.com')
+    a = @user.dup
+    a.username = '          '
     assert_not a.valid?
-    b = User.new(username: 'test_user', name: '         ', email: 'test@example.com')
+    b = @user.dup
+    b.name = '              '
     assert_not b.valid?
-    c = User.new(username: 'test_user', name: 'Test User', email: '                ')
+    c = @user.dup
+    c.email = '             '
     assert_not c.valid?
   end
 
   test 'should have capped length' do
-    a = User.new(username: 'a'*21,      name: 'Test User', email: 'test@example.com')
+    a = @user.dup
+    a.username = 'a'*21
     assert_not a.valid?
-    b = User.new(username: 'test_user', name: 'a'*101,     email: 'test@example.com')
+    b = @user.dup
+    b.name = 'a'*101
     assert_not b.valid?
-    c = User.new(username: 'test_user', name: 'Test User', email: 'a'*244 + '@example.com')
+    c = @user.dup
+    c.email = 'a'*244 + '@example.com'
     assert_not c.valid?
   end
 
   test 'email format should be valid' do
     valid_addresses = %w(user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn)
     valid_addresses.each do |address|
-      a = User.new(username: 'test_user', name: 'Test User', email: address)
+      a = @user.dup
+      a.email = address
       assert a.valid?, "#{address.inspect} should be valid"
     end
     invalid_addresses = %w(user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz..com)
     invalid_addresses.each do |address|
-      a = User.new(username: 'test_user', name: 'Test User', email: address)
+      a = @user.dup
+      a.email = address
       assert_not a.valid?, "#{address.inspect} should be invalid"
     end
   end
 
   test 'enforce uniqueness' do
-    @user = User.new(username: 'test_user', name: 'Test User', email: 'test@example.com')
-    @user.save
-    dup1 = User.new(username: 'blahblah', name: 'Blah Blah', email: @user.email.upcase)
+    a = @user.dup
+    a.save
+    dup1 = User.new(username: 'blahblah', name: 'Blah Blah', email: @user.email.upcase,
+                    password: 'a'*20, password_confirmation: 'a'*20)
     assert_not dup1.valid?
-    dup2 = User.new(username: @user.username, name: 'Blah Blah', email: 'blah@blah.blah')
+    dup2 = User.new(username: @user.username, name: 'Blah Blah', email: 'blah@blah.blah',
+                    password: 'a'*20, password_confirmation: 'a'*20)
     assert_not dup2.valid?
   end
 
   test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
-    a = User.new(username: 'test_user', name: 'Test User', email: mixed_case_email)
+    a = @user.dup
+    a.email = mixed_case_email
     a.save
     assert_equal mixed_case_email.downcase, a.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    a = @user.dup
+    a.password = a.password_confirmation = " " * 6
+    assert_not a.valid?
+  end
+
+  test "password should have a minimum length" do
+    a = @user.dup
+    a.password = a.password_confirmation = "a" * 5
+    assert_not a.valid?
   end
 end
