e4479f7e78edd10af4dab472611896f17581bd87
Alexander Shoup <shoup_alex@yahoo.com>
2017-02-26 23:09:03 -0800

    better user validation w/ tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :username, length: { maximum: 12 }, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { email.downcase! }
+
+  validates :username, length: { maximum: 20 },  presence: true, uniqueness: { case_sensitive: true }
   validates :name,     length: { maximum: 100 }, presence: true
-  validates :email,    presence: true
+  validates :email,    length: { maximum: 255 }, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
