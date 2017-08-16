2ac6813dbe0ec0b3df7044b82a43af9d5b3a1e80
Alexander Shoup <shoup_alex@yahoo.com>
2017-08-15 22:26:01 -0700

    finish basic sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,12 @@ class User < ApplicationRecord
   validates :name,     length: { maximum: 100 }, presence: true
   validates :email,    length: { maximum: 255 }, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6, maximum: 63 }, presence: true
-
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
